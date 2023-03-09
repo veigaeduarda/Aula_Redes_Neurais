6a6e4884a63d6a875826a4d979050667eb8c42a3
Eduarda Veiga <107067566+veigaeduarda@users.noreply.github.com>
2023-03-09 17:48:07 -0300

    Aula 1- parte 2

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,12 @@
- 
+def funcao_objetivo_cb (individuo):
+    """ Computa a função objetivo no problema das caixas binárias.
+    
+    Args:
+        n: número de genes dos indivíduo.
+    
+    Return:
+        Um valor representando a soma dos genes do individuo.
+    """
+    
+    return sum (individuo)
+

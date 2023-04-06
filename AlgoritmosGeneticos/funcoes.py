f4844a28379154b44e2bc3b4d6b8135a98abe127
Eduarda Veiga <107067566+veigaeduarda@users.noreply.github.com>
2023-04-06 17:33:24 -0300

    aula 06/04

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,6 +1,49 @@
 import random
 
 ###############################################################################
+#                                   Suporte                                   #
+##############################################################################+
+
+
+# NOVIDADE
+def distancia_entre_dois_pontos(a, b):
+    """Computa a distância Euclidiana entre dois pontos em R^2
+    Args:
+      a: lista contendo as coordenadas x e y de um ponto.
+      b: lista contendo as coordenadas x e y de um ponto.
+    Returns:
+      Distância entre as coordenadas dos pontos `a` e `b`.
+    """
+
+    x1 = a[0]
+    x2 = b[0]
+    y1 = a[1]
+    y2 = b[1]
+
+    dist = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
+
+    return dist
+
+
+# NOVIDADE
+def cria_cidades(n):
+    """Cria um dicionário aleatório de cidades com suas posições (x,y).
+    Args:
+      n: inteiro positivo
+        Número de cidades que serão visitadas pelo caixeiro.
+    Returns:
+      Dicionário contendo o nome das cidades como chaves e a coordenada no plano
+      cartesiano das cidades como valores.
+    """
+
+    cidades = {}
+
+    for i in range(n):
+        cidades[f"Cidade {i}"] = (random.random(), random.random())
+
+    return cidades
+
+###############################################################################
 #                   Funções para os  Genes                                    #
 ###############################################################################
 
@@ -116,6 +159,21 @@ def individuo_desafio(mini, maxi):
 
     return candidato
 
+# NOVIDADE
+def individuo_cv(cidades):
+    """Sorteia um caminho possível no problema do caixeiro viajante
+    Args:
+      cidades:
+        Dicionário onde as chaves são os nomes das cidades e os valores são as
+        coordenadas das cidades.
+    Return:
+      Retorna uma lista de nomes de cidades formando um caminho onde visitamos
+      cada cidade apenas uma vez.
+    """
+    nomes = list(cidades.keys())
+    random.shuffle(nomes)
+    return nomes
+
 ###############################################################################
 #                   Funções para populações                                   #
 ###############################################################################
@@ -184,6 +242,23 @@ def populacao_inicial_desafio(tamanho, mini, maxi, letras):
         populacao.append(individuo_senha(tamanho_senha, letras))
     return populacao
 
+# NOVIDADE
+def populacao_inicial_cv(tamanho, cidades):
+    """Cria população inicial no problema do caixeiro viajante.
+    Args
+      tamanho:
+        Tamanho da população.
+      cidades:
+        Dicionário onde as chaves são os nomes das cidades e os valores são as
+        coordenadas das cidades.
+    Returns:
+      Lista com todos os indivíduos da população no problema do caixeiro
+      viajante.
+    """
+    populacao = []
+    for _ in range(tamanho):
+        populacao.append(individuo_cv(cidades))
+    return populacao
 
 
 ###############################################################################
@@ -306,6 +381,7 @@ def funcao_objetivo_senha(individuo, senha_verdadeira):
 
     return diferenca
 
+#desafio
 def funcao_objetivo_senha_desafio(individuo, senha_verdadeira):
     """Computa a funcao objetivo de um individuo no problema da senha
     Args:
@@ -326,6 +402,40 @@ def funcao_objetivo_senha_desafio(individuo, senha_verdadeira):
     
 
     return (diferenca + dif_size)
+
+# NOVIDADE
+def funcao_objetivo_cv(individuo, cidades):
+    """Computa a funcao objetivo de um individuo no problema do caixeiro viajante.
+    Args:
+      individiuo:
+        Lista contendo a ordem das cidades que serão visitadas
+      cidades:
+        Dicionário onde as chaves são os nomes das cidades e os valores são as
+        coordenadas das cidades.
+    Returns:
+      A distância percorrida pelo caixeiro seguindo o caminho contido no
+      `individuo`. Lembrando que após percorrer todas as cidades em ordem, o
+      caixeiro retorna para a cidade original de onde começou sua viagem.
+    """
+
+    distancia = 0
+
+    for posicao in range(len(individuo) - 1):
+        
+        partida = cidades[individuo[posicao]]
+        chegada = cidades[individuo[posicao + 1]]
+        
+        percurso = distancia_entre_dois_pontos(partida, chegada)
+        distancia = distancia + percurso        
+               
+    # Calculando o caminho de volta para a cidade inicial
+    partida = cidades[individuo[-1]]
+    chegada = cidades[individuo[0]]
+
+    percurso = distancia_entre_dois_pontos(partida, chegada)
+    distancia = distancia + percurso
+    
+    return distancia
 
 ###############################################################################
 #              Funções objetivo para populações                               #
@@ -390,6 +500,24 @@ def funcao_objetivo_pop_senha_desafio(populacao, senha_verdadeira):
     return resultado
 
 
+# NOVIDADE
+def funcao_objetivo_pop_cv(populacao, cidades):
+    """Computa a funcao objetivo de uma população no problema do caixeiro viajante.
+    Args:
+      populacao:
+        Lista com todos os inviduos da população
+      cidades:
+        Dicionário onde as chaves são os nomes das cidades e os valores são as
+        coordenadas das cidades.
+    Returns:
+      Lista contendo a distância percorrida pelo caixeiro para todos os
+      indivíduos da população.
+    """
+
+    resultado = []
+    for individuo in populacao:
+        resultado.append(funcao_objetivo_cv(individuo, cidades))
+    return resultado
 
 
 
@@ -439,6 +567,41 @@ def cruzamento_ponto_simples_desafio (pai, mae):
     filho2 = mae [ : ponto_de_corte] + pai [ponto_de_corte :]
     
     return filho1, filho2
+
+
+# NOVIDADE
+def cruzamento_ordenado(pai, mae):
+    """Operador de cruzamento ordenado.
+    Neste cruzamento, os filhos mantém os mesmos genes que seus pais tinham,
+    porém em uma outra ordem. Trata-se de um tipo de cruzamento útil para
+    problemas onde a ordem dos genes é importante e não podemos alterar os genes
+    em si. É um cruzamento que pode ser usado no problema do caixeiro viajante.
+    Ver pág. 37 do livro do Wirsansky.
+    Args:
+      pai: uma lista representando um individuo
+      mae : uma lista representando um individuo
+    Returns:
+      Duas listas, sendo que cada uma representa um filho dos pais que foram os
+      argumentos. Estas listas mantém os genes originais dos pais, porém altera
+      a ordem deles
+    """
+    corte1 = random.randint(0, len(pai) - 2)
+    corte2 = random.randint(corte1 + 1, len(pai) - 1)
+    
+    filho1 = pai[corte1:corte2]
+    for gene in mae:
+        if gene not in filho1:
+            filho1.append(gene)
+            
+    filho2 = mae[corte1:corte2]
+    for gene in pai:
+        if gene not in filho2:
+            filho2.append(gene)
+            
+    return filho1, filho2
+
+        
+
 
 
 ###############################################################################
@@ -502,6 +665,33 @@ def mutacao_size(individuo):
         
     return individuo
 
+# NOVIDADE
+def mutacao_de_troca(individuo):
+    """Troca o valor de dois genes.
+    Args:
+      individuo: uma lista representado um individuo.
+    Return:
+      O indivíduo recebido como argumento, porém com dois dos seus genes
+      trocados de posição.
+    """
+    
+    indices = list (range(len(individuo)))
+    lista_sorteada = random.sample (indices, k=2) #amostragem
+    indice1 = lista_sorteada[0]
+    indice2 = lista_sorteada[1]
+    
+    #individuo[indice1], individuo[indice2] = individuo [indice2], individuo [indice1]
+    
+    gene1 = individuo[indice1]
+    gene2 = individuo[indice2]
+    
+    gene2 = individuo[indice1]
+    gene1 = individuo[indice2]
+    
+    
+    return individuo
+    
+                    
                                 
 ###############################################################################
 #                   Funções da atividade 6                                    #
